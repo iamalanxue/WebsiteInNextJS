aba08d67931f58fa6e2d5813c3b515e092190f42
iamalanxue <alanxue43@gmail.com>
2023-02-22 21:03:38 -0800

    initial commit

---
+++ components/LandingPage.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Logo from "./Logo";
+
+const LandingPage = () => {
+  
+  return (
+    <div className="landing-page">
+      <Logo/>
+    </div>
+  );
+}
+ 
+export default LandingPage;
